22997e1a66e972bc9a5a08b82fc70a8070b544ba
Stephen Mesa <stephen.mesa@gmail.com>
2021-12-05 20:40:53 -0800

    Day 6 setup

---
+++ day06Driver.mjs
@@ -0,0 +1,12 @@
+import fs from 'fs';
+
+import {
+    calc1,
+    calc2,
+    parseInput
+} from './day06.mjs';
+
+const input = parseInput(fs.readFileSync('inputs/day06.txt').toString());
+
+console.log('Day 06 - First answer', calc1(input));
+console.log('Day 06 - Second answer', calc2(input));
