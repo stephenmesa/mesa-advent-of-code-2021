3bb75b2fbf4e726c4982be6de2873e23eb5db7cb
Stephen Mesa <stephen.mesa@gmail.com>
2021-12-09 20:52:34 -0800

    Day 10 setup

---
+++ test/day10.test.mjs
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import assert from 'assert';
+
+import {
+    calc1,
+    calc2,
+    parseInput,
+} from '../day10.mjs';
+
+const testInput = parseInput(fs.readFileSync('inputs/day10-simple.txt').toString());
+
+describe('Day 10', () => {
+    it('First problem', () => {
+        const target = calc1(testInput);
+        assert.equal(target, -1);
+    });
+
+    it.skip('Second problem', () => {
+        const target = calc2(testInput);
+        assert.equal(target, -1);
+    });
+});
