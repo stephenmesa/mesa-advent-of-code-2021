5ad45a8de9d6398ad37e01228488897bb08c60e7
Stephen Mesa <stephen.mesa@gmail.com>
2021-12-12 00:23:18 -0800

    day 12

--- day12.mjs
+++ day12.mjs
@@ -1,7 +1,71 @@
-export const parseInput = (input) => input.split('\n');
+export const parseInput = (input) => input.split('\n').map(l => l.split('-'));
 
-export const calc1 = (input) => {
+const cannotRevisitCave = (caveName, caveAllowedRevisit) => {
+    if (caveName === 'start' || caveName === 'end') {
+        return {
+            revisit: false,
+            usedRevisit: false,
+        };
+    } else if (caveName.match(/[A-Z]+/g)) {
+        return {
+            revisit: true,
+            usedRevisit: false,
+        };
+    } else if (caveName === caveAllowedRevisit) {
+        return {
+            revisit: true,
+            usedRevisit: true,
+        };
+    } else {
+        return {
+            revisit: false,
+            usedRevisit: false,
+        };
+    }
 };
 
+const findPaths = (caves, start, end, revisitCave = null) => {
+    const possibleNextSteps = caves.filter(p => p[0] === start || p[1] === start);
+    let caveAllowedRevisit = revisitCave;
+    const revisitPolicy = cannotRevisitCave(start, caveAllowedRevisit);
+    if (revisitPolicy.usedRevisit) {
+        caveAllowedRevisit = null;
+    }
+    const nextCaves = revisitPolicy.revisit ? [...caves] : caves.filter(p => p[0] !== start && p[1] !== start);
+    if (possibleNextSteps.length === 0) {
+        return [];
+    }
+
+    const paths = [];
+    possibleNextSteps.forEach(s => {
+        const nextCave = s[0] === start ? s[1] : s[0];
+        if (nextCave === end) {
+            paths.push([start, nextCave]);
+        } else {
+            // Remove this step if it's a small cave
+            const nextPaths = findPaths(nextCaves, nextCave, end, caveAllowedRevisit);
+            paths.push(...nextPaths.map(np => [start, ...np]));
+        }
+    });
+
+    return paths;
+};
+
+export const calc1 = (input) => {
+    const paths = findPaths(input, 'start', 'end');
+    return paths.length;
+};
+
+const unique = (value, index, self) => {
+    return self.indexOf(value) === index
+}
+
 export const calc2 = (input) => {
+    const allSmallCaves = input.reduce((acc, row) => [...acc, ...row], []).filter(unique).filter(c => c !== 'start' && c !== 'end' && !!c.match(/[a-z]+/g));
+
+    const diffPaths = allSmallCaves.map(c => findPaths(input, 'start', 'end', c));
+    
+    const allPaths = diffPaths.reduce((acc, a) => ([...acc, ...a]), []);
+
+    return allPaths.map(x => x.join('')).filter(unique).length;
 };
--- test/day12.test.mjs
+++ test/day12.test.mjs
@@ -7,16 +7,42 @@ import {
     parseInput,
 } from '../day12.mjs';
 
-const testInput = parseInput(fs.readFileSync('inputs/day12-simple.txt').toString());
+const testInput1 = parseInput(fs.readFileSync('inputs/day12-simple1.txt').toString());
+const testInput2 = parseInput(fs.readFileSync('inputs/day12-simple2.txt').toString());
+const testInput3 = parseInput(fs.readFileSync('inputs/day12-simple3.txt').toString());
 
 describe('Day 12', () => {
-    it('First problem', () => {
-        const target = calc1(testInput);
-        assert.equal(target, -1);
+    describe('First problem', () => {
+        it('Variant 1', () => {
+            const target = calc1(testInput1);
+            assert.equal(target, 10);
+        });
+
+        it('Variant 2', () => {
+            const target = calc1(testInput2);
+            assert.equal(target, 19);
+        });
+
+        it('Variant 3', () => {
+            const target = calc1(testInput3);
+            assert.equal(target, 226);
+        });
     });
 
-    it.skip('Second problem', () => {
-        const target = calc2(testInput);
-        assert.equal(target, -1);
+    describe('Second problem', () => {
+        it('Variant 1', () => {
+            const target = calc2(testInput1);
+            assert.equal(target, 36);
+        });
+
+        it('Variant 2', () => {
+            const target = calc2(testInput2);
+            assert.equal(target, 103);
+        });
+
+        it('Variant 3', () => {
+            const target = calc2(testInput3);
+            assert.equal(target, 3509);
+        });
     });
 });
